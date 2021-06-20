ce9f3825c63b444116ef6591d211b65f2dd74b5e
VikkiSinghs <svkram46@gmail.com>
2021-06-21 02:19:10 +0530

    chat screen and navigation

--- components/ChatListItem/index.tsx
+++ components/ChatListItem/index.tsx
@@ -1,35 +1,46 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View , Text , Image , TouchableWithoutFeedback } from "react-native";
 import { ChatRoom } from "../../types";
 import styles from "./style";
 import moment from 'moment';
-
+import {useNavigation} from '@react-navigation/native';
 
 export type ChatListItemProps = {
-    chatRoom: chatRoom;
+    chatRoom: ChatRoom;
 }
 
 
 const ChatListItem = (props: ChatListItemProps) => {
     const { chatRoom } = props;
-    const user = chatRoom.users[0];
+    const navigation = useNavigation();
+    const user = chatRoom.users[1];
+
+
+    const onClick = () => {
+        navigation.navigate('ChatRoom', {
+            id: chatRoom.id,
+            name: user.name,
+    })
+    }
 
     return (
+        <TouchableWithoutFeedback onPress={onClick}>
         <View style = {styles.container}>
                 <View style={styles.leftContainer}>
-                    <Image source={{uri: user.imageUri}} style={styles.avatar}/>
+                    <Image source={{ uri: user.imageUri }} style={styles.avatar}/>
 
                     <View style={styles.midContainer}>
                         <Text style={styles.username}>{user.name}</Text>
-                        <Text numberOfLines={1} style={styles.lastMessage}>{chatRoom.lastMessage.content}</Text>
+                        <Text numberOfLines={2} style={styles.lastMessage}>{chatRoom.lastMessage.content}</Text>
                          
                     </View>
                 </View>
 
-            <Text style={styles.time}>
-                {moment(chatRoom.lastMessage.createdAt).format("YYYY/MM/DD")}
+                <Text style={styles.time}>
+                  {moment(chatRoom.lastMessage.createdAt).format("ddd,ha")}
             </Text>
         </View>
+        </TouchableWithoutFeedback>
     )
 
 };
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -9,12 +9,13 @@ import * as React from 'react';
 import { ColorSchemeName, View } from 'react-native';
 import Colors from "../constants/Colors";
 // import view from "react-native";
-import { Octicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { Octicons, MaterialCommunityIcons , MaterialIcons, FontAwesome5,  } from '@expo/vector-icons';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../types';
 import MainTabNavigator from './MainTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
-
+import ChatRoomScreen from "../screens/ChatRoomScreen";
+import { TabRouter } from 'react-navigation';
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
   return (
     <NavigationContainer
@@ -61,10 +62,29 @@ function RootNavigator() {
           )
         }}
       />
-      <Stack.Screen
-        name="NotFound"
-        component={NotFoundScreen}
-        options={{ title: 'Oops!' }} />
-    </Stack.Navigator>
+<Stack.Screen
+        name="ChatRoom"
+        component={ChatRoomScreen}
+        options={({route}) => ({
+          title: route.params.name,
+          headerRight: () => (
+            <View style={{
+              flexDirection: 'row',
+              width: 100,
+              justifyContent: 'space-between',
+              marginRight: 10,
+            }}>
+              <MaterialIcons name= "call" size={22} color={'white'} />
+              <FontAwesome5 name= "video" size={22} color={'white'} />
+              <MaterialCommunityIcons name= "dots-vertical" size={22} color={'white'} />
+            </View>
+          )
+          })}
+           />
+    
+       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+       </Stack.Navigator>
+      
+      
   );
 }
